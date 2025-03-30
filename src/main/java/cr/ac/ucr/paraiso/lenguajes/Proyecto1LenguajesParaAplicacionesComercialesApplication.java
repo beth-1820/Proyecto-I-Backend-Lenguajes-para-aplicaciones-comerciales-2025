0b429db170625264afe686a7f565630e0bd837e4
beth-1820 <beth_1820@outlook.com>
2025-03-29 22:56:15 -0600

    Initial commit - Proyecto Spring Boot

---
+++ src/main/java/cr/ac/ucr/paraiso/lenguajes/Proyecto1LenguajesParaAplicacionesComercialesApplication.java
@@ -0,0 +1,13 @@
+package cr.ac.ucr.paraiso.lenguajes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto1LenguajesParaAplicacionesComercialesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyecto1LenguajesParaAplicacionesComercialesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cr/ac/ucr/paraiso/lenguajes/Proyecto1LenguajesParaAplicacionesComercialesApplicationTests.java
@@ -0,0 +1,13 @@
+package cr.ac.ucr.paraiso.lenguajes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyecto1LenguajesParaAplicacionesComercialesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
